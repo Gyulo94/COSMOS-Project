e3d9c0cc9ed076fc860ed396a40cb4980fbbfa24
Gyulo94 <qkrcjfrb112@gmail.com>
2023-08-21 18:52:07 +0900

    첫번째 커밋

---
+++ COSMOS-Project/src/main/java/com/cosmos/CosmosProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cosmos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmosProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosmosProjectApplication.class, args);
+	}
+
+}
---
+++ COSMOS-Project/src/main/java/com/cosmos/board/controller/boardcontroller.java
@@ -0,0 +1,8 @@
+package com.cosmos.board.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class boardcontroller {
+
+}
